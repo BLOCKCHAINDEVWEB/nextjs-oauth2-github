98ace6be1f3465e9c3ec9d2118ba41689a52fe8f
httpcode <blockchaindevweb@gmail.com>
2022-02-06 17:42:01 +0100

    fix bug logout

--- components/Navbar.js
+++ components/Navbar.js
@@ -12,12 +12,8 @@ export default function Navbar({ session, location }) {
       <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
         <div className="relative flex items-center justify-between h-16">
           <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            {/* Mobile menu button */}
             <button type="button" className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false" onClick={() => setIsOpenMenuMobile(!isOpenMenuMobile)}>
               <span className={isOpenMenuMobile ? 'sr-only' : ''}>Open main menu</span>
-                {/* Icon when menu is closed.
-                Heroicon name: outline/menu
-                Menu open: "hidden", Menu closed: "block" */}
               {isOpenMenuMobile
                 ? <svg className="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16" />
@@ -38,7 +34,6 @@ export default function Navbar({ session, location }) {
             {!isOpenMenuMobile
               ? <div className="hidden sm:block sm:ml-6">
                   <div className="flex space-x-4">
-                    {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
                     <Link href="/home">
                       {location === '/home'
                         ? <a className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium active" aria-current="page">Home</a>
@@ -64,7 +59,6 @@ export default function Navbar({ session, location }) {
               </svg>
             </button>
 
-            {/* Profile dropdown */}
             <div className="ml-3 relative">
               <div>
                 <button type="button" className="text-gray-400 flex items-center justify-center text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu-button" aria-expanded="false" aria-haspopup="true" onClick={() => setIsOpenMenuLapstop(!isOpenMenuLapstop)}>
@@ -77,22 +71,14 @@ export default function Navbar({ session, location }) {
                   }
                 </button>
               </div>
-                {/* Dropdown menu, show/hide based on menu state.
-                Entering: "transition ease-out duration-100"
-                  From: "transform opacity-0 scale-95"
-                  To: "transform opacity-100 scale-100"
-                Leaving: "transition ease-in duration-75"
-                  From: "transform opacity-100 scale-100"
-                  To: "transform opacity-0 scale-95" */}
               {isOpenMenuLapstop
                 ? <div className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" orientation="vertical" aria-labelledby="user-menu-button" tabIndex="-1">
-                    {/* Active: "bg-gray-100", Not Active: "" */}
                     <a href="/profile" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-0">Your Profile</a>
                     <a href="/settings" className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-1">Settings</a>
                     {!session
                       ? <a href={'/'} className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-2">Login</a>
                       : <>
-                          <a href={'/home'} className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-2">
+                          <div className="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabIndex="-1" id="user-menu-item-2">
                             <form action="/api/auth/signout" method="POST">
                               <div>
                                 <button type="submit" onClick={() => signOut('github')}>
@@ -100,7 +86,7 @@ export default function Navbar({ session, location }) {
                                 </button>
                               </div>
                             </form>
-                          </a>
+                          </div>
                       </>
                     }
                   </div>
@@ -111,11 +97,9 @@ export default function Navbar({ session, location }) {
         </div>
       </div>
 
-      {/* Mobile menu, show/hide based on menu state. */}
       {isOpenMenuMobile
         ? <div className="sm:hidden" id="mobile-menu">
             <div className="px-2 pt-2 pb-3 space-y-1">
-              {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
               <Link href="/home">
                 <a className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium" aria-current="page">Home</a>
               </Link>
